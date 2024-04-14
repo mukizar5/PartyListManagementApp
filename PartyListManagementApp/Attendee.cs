e5bf8e730c33dac195db555747e5004f05848a4e
Mukizar5 <mukizar5@gmail.com>
2024-04-14 21:53:22 +0300

    calculating total party fees, assigning fees based on package or status paid for by attendees, arrangement of code.

--- PartyListManagementApp/Attendee.cs
+++ PartyListManagementApp/Attendee.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PartyAttendeesManagementApp.Enums;
+using PartyAttendeesManagementApp.Helpers;
 
 namespace PartyAttendeesManagementApp
 {
-    public class Attendee
+    public class Attendee:BaseClass
     {
         public string Name { get; set; }
 
-        public double PartyFees { get; set; }
+        public PartyFees PartyFees { get; set; }
 
-        public DateTime TimeOfArrival { get; set; }
+
+        
     }
 }
---
+++ PartyListManagementApp/Enums/PartyFees.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace PartyAttendeesManagementApp.Enums
+{
+    public enum PartyFees
+    {
+        VVIP = 0,
+        VIP = 1,
+        Couples = 2,
+        Singles = 3,
+    }
+}
